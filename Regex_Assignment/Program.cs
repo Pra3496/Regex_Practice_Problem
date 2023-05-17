51b2c68cb6006218da1ae383503ab833dfa97a90
Pranav Waghmare <piyushw55@gmail.com>
2023-05-17 16:58:01 +0530

    [Pranav]Added: First Pattern

---
+++ Regex_Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Regex_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexPattern reg = new RegexPattern();
+
+            reg.UCFirst();
+          
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Regex_Assignment/RegexPattern.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Regex_Assignment
+{
+    internal class RegexPattern
+    {
+        public void UCFirst()
+        {
+            string pattern = "^[a]{1}[b]{2,}";
+           
+
+            string Name = Console.ReadLine();
+
+            if (Regex.IsMatch(Name, pattern))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Not Valid");
+            }
+
+            Console.ReadKey();
+
+        }
+
+      
+    }
+}
